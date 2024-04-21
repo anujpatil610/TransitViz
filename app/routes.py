d30b5a2357840c529741642ac35381ec8eaa3806
anujpatil610 <anpatil@clarkson.edu>
2024-04-21 18:49:31 -0400

    Test2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template
 from app import app
 from app.utils.gtfs_processor import get_routes, get_stops, get_stop_times, get_trips
+from app.utils.gtfs_processor import get_weekly_timetable_by_route_name
+from flask import render_template, request
+from app.utils.gtfs_processor import get_all_route_names
+
 
 @app.route('/')
 def index():
@@ -22,7 +26,20 @@ def gtfs_dashboard():
 
 # Additional routes can be created for more detailed data pages or API endpoints.
 
+@app.route('/timetable', methods=['GET'])
+def timetable():
+    route_names = get_all_route_names()
+    route_name = request.args.get('route_name', None)
+    timetable_html = ""
 
+    if route_name:
+        weekly_timetable = get_weekly_timetable_by_route_name(route_name)
+        if not weekly_timetable.empty:
+            timetable_html = weekly_timetable.to_html(classes='table table-responsive')
+        else:
+            timetable_html = "<div class='alert alert-warning'>No timetable available for this route.</div>"
+
+    return render_template('timetable.html', route_names=route_names, timetable_html=timetable_html)
 
 @app.route('/gps-dashboard')
 def gps_dashboard():
--- app/utils/gtfs_processor.py
+++ app/utils/gtfs_processor.py
@@ -23,4 +23,30 @@ def get_trips():
     """Returns the trips DataFrame."""
     return load_gtfs_file('trips.txt')
 
+# ... (other functions)
+
+def get_weekly_timetable_by_route_name(route_long_name):
+    routes = get_routes()
+    trips = get_trips()
+    stop_times = get_stop_times()
+    calendar = load_gtfs_file('calendar.txt')
+
+    route_id = routes[routes['route_long_name'] == route_long_name]['route_id'].values
+    if route_id.size == 0:
+        return pd.DataFrame()  # No such route found
+
+    trips_for_route = trips[trips['route_id'].isin(route_id)]
+    schedule = pd.merge(stop_times, trips_for_route, on='trip_id')
+    weekly_schedule = pd.merge(schedule, calendar, on='service_id')
+
+    return weekly_schedule
+
+
+
+def get_all_route_names():
+    """Returns a list of all route long names."""
+    routes_df = get_routes()
+    return routes_df['route_long_name'].tolist()
+
+
 # Additional functions can be defined to process data for specific visualizations or statistics.
